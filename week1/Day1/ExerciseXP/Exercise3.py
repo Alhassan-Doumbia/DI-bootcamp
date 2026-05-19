05501b290c8dee1238abd44e547ab8d8108b9d1a
Al Hassan Doumbia <hassan4cod@gmail.com>
2026-05-19 15:16:24 +0000

    Exercise 3: What is the output

---
+++ week1/Day1/ExerciseXP/Exercise3.py
@@ -0,0 +1,12 @@
+# Instructions
+# Predict the output of the following code snippets:
+# Coment what is your guess, then run the code and compare
+
+
+
+>>> 5 < 3 #False
+>>> 3 == 3 #True
+>>> 3 == "3" #False
+>>> "3" > 3 #error
+>>> "Hello" == "hello"#False
+
