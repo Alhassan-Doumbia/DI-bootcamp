4c17eb52f1d7d0d4ec97602d7f1864af1f81aa96
Al Hassan Doumbia <hassan4cod@gmail.com>
2026-05-19 15:17:25 +0000

    Exercise 7: Odd or Even

---
+++ week1/Day1/ExerciseXP/Exercise7.py
@@ -0,0 +1,9 @@
+#Instructions
+#Write code that asks the user for a number and determines whether this number is odd or even.
+
+user_input = int(input("enter a number :"))
+if user_input%2==0:
+    print("the number is even")
+else:
+    print("the number is odd")
+
