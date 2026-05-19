d7a5711722c879cebb82fc3c4f76b85067589c0a
Al Hassan Doumbia <hassan4cod@gmail.com>
2026-05-19 15:17:10 +0000

    Exercise 6: A & B

---
+++ week1/Day1/ExerciseXP/Exercise6.py
@@ -0,0 +1,8 @@
+# Instructions
+# Create two variables, a and b.
+# Each variable’s value should be a number.
+# If a is bigger than b, have your code print "Hello World"
+a=6
+b=4
+if a>b:
+    print("hello world")
